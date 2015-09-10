6156367e91f3a39e19dfc146cb65bc3858b9c659
Bruno Sendras <brunosendras@gmail.com>
2015-09-09 23:13:21 -0300

    - Completados los mensajes de usuario faltantes. - Coregida la linea "!test $1 -r" por "!(test -r $1)" debido a que fallaba   por la precedencia del operador '!'' y por el orden de los parametros   del comando "test".

--- Ejercicio1/ejercicio1.sh
+++ Ejercicio1/ejercicio1.sh
@@ -16,34 +16,72 @@
 #
 #**************************************************************************************************
 
-ErrorS()
-{
-echo "Error. La sintaxis del script es la siguiente:"
-echo "......................: $0 nombre_archivo L" #COMPLETAR
-echo "......................: $0 nombre_archivo C" #COMPLETAR
-echo "......................: $0 nombre_archivo M" #COMPLETAR
+#**************************************************************************************************
+# CODIGO ORIGINAL COMENTADO.
+#**************************************************************************************************
+##	ErrorS()
+##	{
+##	echo "Error. La sintaxis del script es la siguiente:"
+##	echo "......................: $0 nombre_archivo L" #COMPLETAR
+##	echo "......................: $0 nombre_archivo C" #COMPLETAR
+##	echo "......................: $0 nombre_archivo M" #COMPLETAR
+##	}
+##	
+##	ErrorP()
+##	{
+##	echo "Error. nombre_archivo ....................." #COMPLETAR
+##	}
+##	
+##	if test $# -lt 2; then
+##		ErrorS
+##	fi
+##	if !test $1 -r; then
+##		ErrorP
+##	elif test -f $1 && (test $2 = "L" || test $2 = "C" || test $2 = "M"); then
+##		if test	$2 = "L"; then
+##			res=`wc -l $1`
+##			echo "...............................: $res" #COMPLETAR
+##		elif test $2 = "C"; then
+##			res=`wc -m $1`
+##			echo "...............................: $res" #COMPLETAR
+##		elif test $2 = "M"; then
+##			res=`wc -L $1`
+##			echo "...............................: $res" #COMPLETAR
+##		fi
+##	else
+##		ErrorS
+##	fi
+
+#**************************************************************************************************
+# NUESTRAS MODIFICACIONES.
+#**************************************************************************************************
+
+ErrorS() {
+	echo "Error. La sintaxis del script es la siguiente:"
+	echo "Para ver el numero de lineas que contiene el archivo 			: $0 nombre_archivo L" # COMPLETADO
+	echo "Para ver el numero caracteres que contiene el archivo 		: $0 nombre_archivo C" # COMPLETADO
+	echo "Para ver la longitud de la linea mas larga del archivo 		: $0 nombre_archivo M" # COMPLETADO
 }
 
-ErrorP()
-{
-echo "Error. nombre_archivo ....................." #COMPLETAR
+ErrorP() {
+	echo "Error. nombre_archivo no existe o no tiene permisos de lectura." # COMPLETADO
 }
 
 if test $# -lt 2; then
 	ErrorS
 fi
-if !test $1 -r; then
+if !(test -r $1); then
 	ErrorP
 elif test -f $1 && (test $2 = "L" || test $2 = "C" || test $2 = "M"); then
 	if test	$2 = "L"; then
 		res=`wc -l $1`
-		echo "...............................: $res" #COMPLETAR
+		echo "Numero de lineas que contiene el archivo: $res" # COMPLETADO
 	elif test $2 = "C"; then
 		res=`wc -m $1`
-		echo "...............................: $res" #COMPLETAR
+		echo "Numero caracteres que contiene el archivo: $res" # COMPLETADO
 	elif test $2 = "M"; then
 		res=`wc -L $1`
-		echo "...............................: $res" #COMPLETAR
+		echo "Longitud de la linea mas larga del archivo: $res" # COMPLETADO
 	fi
 else
 	ErrorS
