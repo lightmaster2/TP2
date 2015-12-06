d5b73b6c5ff3137341af081e53d937e13857c785
lighmaster <lighmaster_lomas@hotmail.com>
2015-12-06 16:10:50 -0300

    Update ejercicio3.sh
    APROBADO! ULTIMO

--- Ejercicio3/ejercicio3.sh
+++ Ejercicio3/ejercicio3.sh
@@ -16,24 +16,30 @@
 #***********************************************************************************************************************
 
 #----------------------- FUNCIONES --------------------------------------------------------------------------------------------
-AyudaScript(){
+AyudaScript()
+{
        
-	echo -e "DESCRIPCION: El siguiente script busca en un directorio o archivo la existencia de scripts. Caso existan, se pasa a mayusculas solo su nombre. Deja notificado en un archivo de log los cambios modificados."
-	echo -e "Debe ser pasado por parametro la carpeta/archivo para analizar y el nombre del archivo log."
+	echo -e "DESCRIPCION: El siguiente script busca en directorio/s o archivo/s pasado por parametros la existencia de scripts BASH. Caso existan, se pasa a mayusculas solo su nombre. Deja notificado en un archivo de log los cambios modificados."
+	echo -e "Debe ser pasado por parametro el nombre del archivo Log y la/s carpetas/archivos para analizar."
+	echo -e "En caso que no tenga Permiso de lectura o escritura, el archivo no será modificado y m ostrará el error en el log."
+	echo -e "En caso que un archivo o carpeta no exista o no tenga permisos sufucientes, sera notificado y continuara procesando con los demas"
     	echo -e "OPCIONES:-?, -help,-h Muestra la informacion de uso del script."
-    	echo -e "EJEMPLOS:\t   $0 /home/lgomez/Documentos/prueba1.sh $1 /home/lgomez/Documentos/logEjercicio3.log"
-		echo -e "EJEMPLOS:\t   $0 ./prueba ./archivo_log.log"
+	echo -e "EJEMPLO:\t   $0 ./[archivo_log.log] [./directorio] [./archivo.sh] [./test/ejer.old] [...]"
+    	echo -e "EJEMPLO:\t   $0 /home/lgomez/Documentos/logEjercicio3.log /home/lgomez/Documentos/prueba1.sh"
+	echo -e "EJEMPLO:\t   $0 ./archivo_log.log ./prueba ./archivo.sh ./test"
 	echo " "
 }
 
-errorParam1(){
-echo No se envio el parametro1, ruta_del_archivo/carpeta.
-echo Para mas ayuda\: $0 -h
+errorParam1()
+{
+	echo "No se envio el archivo de Log."
+	echo "Para mas ayuda\: $0 -h"
 }
 
-errorParam2(){
-echo No se envio el parametro2, ruta_archivo_log
-echo Para mas ayuda\: $0 -h
+errorParam2()
+{
+	echo "No se envio el parametro2, ruta_archivo carpeta"
+	echo "Para mas ayuda\: $0 -h"
 }
 #-----------------------------------------------------------------------------------------------------------------------------
 
@@ -48,90 +54,109 @@ if [[ $1 == "-h" || $1 == "-?" || $1 == "-help" ]]
 fi
 
 #se valida la cantidad de parametros enviada.
-if [[ $# != 2 ]]
+if [[ $# < 2 ]]
 then
-	echo "La cantidad de parametros enviada es incorrecta."
+	echo "La cantidad de parametros minima enviada es incorrecta."
 	echo "Para mas ayuda\: "$0" -h"
 	exit 1
 fi
 
 
 
-if [[ ! $1 ]]
+if [[ ! "$1" ]]
 then
 	errorParam1
 	exit 1
 fi
 
-#Valido si existe la carpeta o el archivo
-if [[ ! -f "${1}" && ! -d "${1}" ]]
-then
-	echo "El archivo/carpeta "$1" no existe."
-	exit 1
-fi
-
-#Validacion de permisos de lectura sobre el archivo
-if [ ! -r "${1}" ]
- then
-    echo "El archivo "$1" no posee permisos de lectura."
-    exit 1
-fi
-
-#Parametro2
-if [[ ! $2 ]]
-then
-	errorParam2
-	exit 1
-fi
 
 #Si no existe el archivo de log lo creo
-if [ ! -f "${2}" ]
+if [ ! -f "${1}" ]
 then
-	touch "${2}" 2>/dev/null
+	touch "${1}" 2>/dev/null
 	#verifico que se halla creado el archivo de log, de lo contrario informo un error
 	if [[ $? != 0 ]]
 	then
-		echo "La ruta "$2" del archivo de log ingresada es incorrecta."
+		echo "La ruta $1 del archivo de log ingresada es incorrecta."
 		exit 1
 	fi
 fi
 
 #Validacion de permisos de lectura/escritura sobre el archivo de log
-if [ ! -r "${2}" ] || [ ! -w "${2}" ]
+if [ ! -r "${1}" ] || [ ! -w "${1}" ]
  then
-    echo "El archivo "$2" no posee los permisos necesarios."
+    echo "El archivo $1 no posee los permisos necesarios."
     exit 1
 fi
+
+#Creo archivos tmp para procesar
+errores=$(mktemp)
+arrayPath=$(mktemp)
+
+#Guardo el archivo de log ya validado
+archivoLog="$1"
+shift
+
+for param in "$@"
+do
+
+#Valido cada uno de los parametros ingresados
+if [[ ! "$param" ]]
+then
+	errorParam2
+	continue
+else
+	if [[ ! -f "${param}" && ! -d "${param}" ]]
+	then
+		echo "El archivo/carpeta $param no existe."
+		continue
+	else
+		#Validacion de permisos de lectura sobre el archivo
+		if [ ! -r "${param}" ]
+		 then
+		    echo "El archivo $param no posee permisos de lectura."
+		    continue
+		fi
+	fi
+fi
+
+
+#Verifico que el archivo sea un script o si el directorio contiene scripts
+#y lo guardo en un archivo temporal
+grep -lir -e "#!.*/bin/bash" "${param}" 2>"$errores"  >> "$arrayPath"
+
+done
+
 #-----------------------------------------------------------------------------------------------------------------------------
 
 ##COMIENZA SCRIPT
-errores=$(mktemp)
+
 #Creo las variables Dia y Hora para agregar al final del archivo.
 DIA=`date +"%d/%m/%Y"`
 HORA=`date +"%H:%M"`
 
-#Verifico que el archivo sea un script o si el directorio contiene scripts
-ES_SCRIPT=$(grep -lirs "#!" "${1}")
-grep -lir "#!" "${1}" 2>$errores >/dev/null
-
 #ANALIZO LOS ERRORES Y LOS AGREGO AL LOG
-errorees=`cat $errores`
+errorees=`cat "$errores"`
+
 IFS=$'\n'
 for ERROR in $errorees ; do
-arch=`echo $ERROR | cut -d' ' -f2 | cut -d':' -f1`
-arch=`readlink -f $arch`
-
-#Validacion de permisos de lectura/escritura sobre el archivo de log. Si hubo un problema por falta de permisos
-if [ ! -r "${arch}" ] && [ ! -w "${arch}" ]; then
-    echo " $DIA $HORA - El archivo "$arch" no posee permisos de lectura y escritura necesarios.">> $2
-else
-	if [ ! -r "${arch}" ]; then
-		echo " $DIA $HORA - El archivo "$arch" no posee permiso de lectura necesario." >> $2
+	arch=`echo "$ERROR" | cut -d':' -f2`
+	
+	#Eliminamos los espacuios de la ruta del archivo despues del grep
+	arch=`echo "$arch" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`
+	arch=`readlink -f "$arch"`
+	
+	#Validacion de permisos de lectura/escritura sobre el archivo de log. Si hubo un problema por falta de permisos
+	if [ ! -r "${arch}" ] && [ ! -w "${arch}" ]; then
+	    echo " $DIA $HORA - El archivo $arch  No posee permisos de lectura y escritura necesarios.">> "$archivoLog"
+	else
+		if [ ! -r "${arch}" ]; then
+			echo " $DIA $HORA - El archivo $arch  No posee permiso de lectura necesario." >> "$archivoLog"
+		fi
+		if [ ! -w "${arch}" ]; then
+			echo " $DIA $HORA - El archivo $arch  No posee permiso de escritura necesario." >> "$archivoLog"
+		fi
 	fi
-	if [ ! -w "${arch}" ]; then
-		echo " $DIA $HORA - El archivo "$arch" no posee permiso de escritura necesario." >> $2
-	fi
-fi
 done
 
 
@@ -141,21 +166,33 @@ OLDIFS=$IFS
 A=-1          
  
 IFS=$'\n'
-for LINEA in $ES_SCRIPT ; do
-  #IFS=' '  # Separador
+RUTAS_PARAM=`cat "$arrayPath"`
+
+for LINEA in $RUTAS_PARAM ; do
   IFS=$'\t'
+
   for VALOR in $LINEA ; do
-    let A=$A+1
     #Ruta completa
-	RUTA[$A]=`readlink -f ${VALOR}`
-	#Nombre con extension
-	NOMBREEXTENSION[$A]=$(basename ${VALOR})
-	#Extension del archivo
-	EXTENSION[$A]=`basename ${VALOR} | cut -d'.' -f2`
-	#Ruta sin nombre de archivo
-	RUTASINNOMBRE[$A]=`dirname ${RUTA[$A]}`
-	#Nombre del archivo en matusculas
-	NOMBRE[$A]=`basename "${VALOR}" | cut -d'.' -f1 | tr [:lower:] [:upper:]`
+	RUTAA=`readlink -f "${VALOR}"`
+	
+	if [ ! -r "$RUTAA" ]; then
+		echo " $DIA $HORA - El archivo $RUTAA   No posee permiso de lectura necesario." >> "$archivoLog"
+	else
+		if [ ! -w "${RUTAA}" ]; then
+			echo " $DIA $HORA - El archivo $RUTAA   No posee permiso de escritura necesario." >> "$archivoLog"
+		else
+			let A=$A+1
+			RUTA[$A]="$RUTAA"
+			#Nombre con extension
+			NOMBREEXTENSION[$A]=$(basename "${VALOR}")
+			#Extension del archivo
+			EXTENSION[$A]=`basename "${VALOR}" | cut -d'.' -f2`
+			#Ruta sin nombre de archivo
+			RUTASINNOMBRE[$A]=`dirname "${RUTA[$A]}"`
+			#Nombre del archivo en matusculas
+			NOMBRE[$A]=`basename "${VALOR}" | cut -d'.' -f1 | tr [:lower:] [:upper:]`
+		fi 
+	fi 
   done
   IFS=$'\n'
 done
@@ -174,8 +211,11 @@ if [ $A -ge 0 ] ; then
 	echo "# El archivo se renombro el dia $DIA $HORA" >> "${RUTA_ARCHIVO_MODIFICADO}"
 	
 	#Logueo la modificacion y los errores.
-	echo " $DIA $HORA - Se modifico el nombre del archivo ${RUTA_ARCHIVO_MODIFICADO}" >> "${2}"
+	echo " $DIA $HORA - Se modifico el nombre del archivo ${RUTA_ARCHIVO_MODIFICADO}" >> "$archivoLog"
   done
 fi
+
+#Se eliminan los archivos temporales
 rm $errores
+rm $arrayPath
 #FIN SCRIPT
